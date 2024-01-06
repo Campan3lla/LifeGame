882f5d06a752e60799097a0dcc53401da7cd6ace
jonah <115192830+Campan3lla@users.noreply.github.com>
2024-01-05 23:53:29 -0500

    Refactored `main` to use a new `ColorCell` implementation storing its own `Color`

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,17 +1,45 @@
 use std::time::{Duration, Instant};
 use pixels::{Pixels, SurfaceTexture};
+use rand::Rng;
 use winit::dpi::LogicalSize;
 use winit::event::{Event, VirtualKeyCode};
 use winit::event_loop::{ControlFlow, EventLoop};
 use winit::window::{Window, WindowBuilder};
 use winit_input_helper::WinitInputHelper;
-use life::{BaseLifeBoard, Cell, ParallelLifeBoard, LifeBoard};
+use life::{BaseLifeBoard, ParallelLifeBoard, LifeBoard, LifeCell};
 
 const SCALE: u32 = 4;  // How many logical pixels correspond to one `LifeCell`
 const WIDTH: u32 = 1920;
 const HEIGHT: u32 = 1080;
 const N_THREADS: u8 = 5;
 const MS_TIME_STEP: u64 = 250;
+
+const DEAD_COLOR: Color = Color(0, 0, 0, 0xff);
+const ALIVE_COLOR: Color = Color(0x30, 0xff, 0xff, 0xff);
+
+#[derive(PartialEq, Clone, Debug)]
+struct Color(u8, u8, u8, u8);
+impl Color {
+    fn to_array(&self) -> [u8; 4] { [self.0, self.1, self.2, self.3] }
+}
+#[derive(PartialEq, Clone, Debug)]
+struct ColorCell { alive: bool, color: Color } impl ColorCell {
+    pub fn gen() -> ColorCell {
+        let alive = rand::thread_rng().gen_bool(0.5);
+        let color = if alive { ALIVE_COLOR } else { DEAD_COLOR };
+        ColorCell { alive, color }
+    }
+} impl LifeCell<ColorCell> for ColorCell {
+    fn is_alive(&self) -> bool { self.alive }
+
+    fn to_alive(&self) -> ColorCell {
+        ColorCell { alive: true, color: ALIVE_COLOR }
+    }
+
+    fn to_dead(&self) -> ColorCell {
+        ColorCell { alive: false, color: DEAD_COLOR }
+    }
+}
 
 fn main() {
     let event_loop = EventLoop::new();
@@ -20,7 +48,6 @@ fn main() {
     let window = initialize_window(&event_loop);
     let mut pixels = initialize_pixel_buffer(&window);
     let mut game = initialize_life_board();
-
     let mut last_frame_time = Instant::now();
 
     event_loop.run(move |event, _, control_flow| {
@@ -52,17 +79,13 @@ fn main() {
     });
 }
 
-fn redraw_world(pixels: &mut Pixels, game: &mut ParallelLifeBoard<Cell>) {
+fn redraw_world(pixels: &mut Pixels, game: &mut ParallelLifeBoard<ColorCell>) {
     let frame = pixels.frame_mut();
     for (i, pixel) in frame.chunks_exact_mut(4).enumerate() {
         let x = i % (WIDTH / SCALE) as usize;
         let y = i / (WIDTH / SCALE) as usize;
-        if let Some(bool) = game.is_cell_alive(x, y) {
-            if bool {
-                pixel.copy_from_slice(&[0xff, 0, 0, 0xff]);
-            } else {
-                pixel.copy_from_slice(&[0xff, 0xff, 0xff, 0xff]);
-            }
+        if let Some(cell) = game.cell_at(x, y) {
+            pixel.copy_from_slice(&cell.color.to_array())
         }
     }
     pixels.render().expect("Unable to render pixel buffer.");
@@ -78,12 +101,12 @@ fn initialize_window(event_loop: &EventLoop<()>) -> Window {
         .unwrap()
 }
 
-fn initialize_life_board() -> ParallelLifeBoard<Cell> {
-    ParallelLifeBoard::<Cell>::from_board(
+fn initialize_life_board() -> ParallelLifeBoard<ColorCell> {
+    ParallelLifeBoard::<ColorCell>::from_board(
         BaseLifeBoard::gen(
             (WIDTH / SCALE) as usize,
             (HEIGHT / SCALE) as usize,
-            Cell::gen
+            ColorCell::gen
         ),
         N_THREADS
     )
